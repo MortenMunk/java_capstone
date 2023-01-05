bba2253ad4ffc1d817fdf1b379470cfb762d637c
MortenMunk <mortenmunk97@gmail.com>
2023-01-05 19:11:18 +0100

    add doc

--- src/com/main/exceptions/BillingAmountCannotExceedBalanceException.java
+++ src/com/main/exceptions/BillingAmountCannotExceedBalanceException.java
@@ -1,3 +1,11 @@
+//      ###################################
+//      ||                               ||
+//      ||     Morten Munk Andersen      ||
+//      ||    Mortan21@student.aau.dk    ||
+//      ||     2022 CAPSTONE PROJECT     ||
+//      ||                               ||
+//      ###################################
+
 package com.main.exceptions;
 
 public class BillingAmountCannotExceedBalanceException extends RuntimeException {
